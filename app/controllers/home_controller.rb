30144fbf8e9183d8694a2567a3888510c49eb320
Ricardo Cordero <40808304+maestro362@users.noreply.github.com>
2022-07-13 22:34:41 -0500

    final
    -Se retorna a la vista del formulario.
    -Se pasa la variable y se valida que exista en al vista para mostrar un alert de cuantos registro contenia el archivo.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,10 +8,10 @@ class HomeController < ApplicationController
   def store
     require 'roo'
     data = Roo::Spreadsheet.open(params['file'].tempfile)
-    cont = 0
+    @cont = 0
     data.each() do |row|
       
-      if cont > 0
+      if @cont > 0
         # abort row[0]
         
         if !client = Client.where(name: row[0]).first
@@ -27,10 +27,11 @@ class HomeController < ApplicationController
         sale = Sale.create(client_id:client.id,seller_id:seller.id,product_id:product.id,price:row[2],number:row[3],total:(row[2] * row[3]))
         # abort puts client.id
       end
-      cont += 1
+      @cont += 1
     end
-   abort
-    render json: data.row(2)[0]
+    render "home/index"
+   # abort
+   #  render json: data.row(2)[0]
     # render json: params['file'].tempfile
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get "/home", to: 'home#index'
+  get "/store", to: 'home#index'
   post "/store", to: 'home#store'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
